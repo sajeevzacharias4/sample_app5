a5f46d5f33f55b6be27d658e5dfe5c3abba96d3f
sajeevzacharias4 <sajeev.zacharias4@gmail.com>
2013-01-14 13:25:04 +0000

    Finish user edit, update, index nd destroy actions

--- spec/support/utilities.rb
+++ spec/support/utilities.rb
@@ -12,14 +12,14 @@ end
 
 
 def valid_signin(user)
-  fill_in "Email",    with: user.email
-  fill_in "Password", with: user.password
+  fill_in "Email",    :with=> user.email
+  fill_in "Password", :with=> user.password
   click_button "Sign in"
 end
 
 RSpec::Matchers.define :have_error_message do |message|
   match do |page|
-    page.should have_selector('div.alert.alert-error', text: message)
+    page.should have_selector('div.alert.alert-error', :text=> message)
   end
 end
 
